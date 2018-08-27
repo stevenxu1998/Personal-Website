be0224721de7ffa65d7ff0e3ae601039cf61d095
stevenxu1998 <sfxu@edu.uwaterloo.ca>
2018-08-27 15:53:55 -0400

    Started with the Js on the project bottuns, added the content and the menu icon on the about me page

---
+++ scripts/projects.js
@@ -0,0 +1,14 @@
+﻿function projects(num) {
+    if (num == 1){
+        document.getElementById("project").innerHTML = '<h1>Html/Css</h1>';
+        document.getElementById("project").innerHTML = '<h1>Html/Css</h1>';
+        //var title = document.createElement('h1');
+        //title.textContent = 'Html/Css';
+    }
+    else if (num == 2) {
+        document.getElementById("project").innerHTML = "";
+    }
+    else if (num == 3) {
+        document.getElementById("project").innerHTML = "";
+    }
+}
